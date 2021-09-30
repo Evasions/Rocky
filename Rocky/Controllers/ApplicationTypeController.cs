40ed5219976185f23570e84e2be4ed4aa2e8de10
LooseControi <mr.sokorev@gmail.com>
2021-09-30 21:03:42 +0300

    Добавьте файлы проекта.

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ApplicationTypeController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _dbContext.AplicationTypes;
+            return View(objList);
+        }
+        //Get create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            _dbContext.AplicationTypes.Add(obj);
+            _dbContext.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _dbContext.Categories;
+            return View(objList);
+        }
+
+        //Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _dbContext.Categories.Add(obj);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
